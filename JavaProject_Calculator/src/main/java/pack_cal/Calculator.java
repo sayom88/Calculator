becdac5206d6537cc82e058f4773884115c38313
sayom88 <sayom88@yahoo.com>
2016-01-17 12:50:50 +0530

    17th Jan-First Code Checkin

---
+++ JavaProject_Calculator/src/main/java/pack_cal/Calculator.java
@@ -0,0 +1,39 @@
+package pack_cal;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Calculator {
+	
+	WebDriver driver = new FirefoxDriver();
+	
+	
+	  @Test
+	   public void app_launch() throws Exception{
+		
+		   
+		   //System.setProperty("webdriver.ie.driver","C://ECLIPSE_WORKSPACE//IEDriverServer.exe");
+		  // driver=new InternetExplorerDriver();
+		   
+		   System.out.println("The WebDriver IS SHOWN HERE::"+driver);
+	  
+	    driver.manage().timeouts().implicitlyWait(10000,TimeUnit.SECONDS);
+	    
+	   
+ 	
+  //Launch websites
+  //driver.navigate().to();
+	
+	//    C:\ECLIPSE_WORKSPACE\Java_Maven
+	    
+driver.get("C:\\ECLIPSE_WORKSPACE\\JavaProject_Calculator\\Calculator1.html");
+	   // driver.get("Calculator1.html");
+  // https://swarnasetu-beta.mybluemix.net/
+  //Maximize the browser
+  driver.manage().window().maximize();
+	  }
+
+}
